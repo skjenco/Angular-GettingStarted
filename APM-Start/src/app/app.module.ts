448b6583dfb4bd99b034c030012262e3fe3e8236
stephenjen <stephenjen@utah.gov>
2018-01-10 10:41:33 -0700

    Using Interfaces to make filters pipes etc

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./Product";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent  {
+export class ProductListComponent implements OnInit{
   pageTitle : string = 'Product List!';
-  products : any[] = [
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage : boolean = false;
+
+  _listFilter : string;
+  get listFilter() : string {
+    return this._listFilter;
+  }
+  set listFilter(value:string){
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.peformFilter(this.listFilter) : this.products;
+  }
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  peformFilter(filterBy: string) : IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product : IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !==-1);
+  }
+
+
+  toggleImage() : void {
+    this.showImage = !this.showImage;
+  };
+
+  ngOnInit() : void {
+    console.log('here on init');
+  }
+
+  filteredProducts : IProduct[];
+
+  products : IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
