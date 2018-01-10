f0f5f5a440e71446c9e8e6304928ce4c487bf6a7
stephenjen <stephenjen@utah.gov>
2018-01-10 13:17:30 -0700

    Creating a nested Component

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
+import {StarComponent} from "./shared/star.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
